c3da457c6fcc1a1aff2638437fc8467d59df12d1
Maria Komarova <komarova@phystech.edu>
2021-05-05 10:25:30 +0300

    Move all in index.js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const express = require('express');
 require('dotenv').config();
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { errors, celebrate, Joi } = require('celebrate');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const auth = require('./middlewares/auth');
-const { login, createUser } = require('./controllers/users');
-const usersRouter = require('./routes/users');
-const moviesRouter = require('./routes/movies');
-const handleErrors = require('./middlewares/errors');
+const router = require('./routes/index')
 
 const app = express();
 
@@ -25,43 +17,6 @@ mongoose.connect(
   }
 );
 
-app.use(cors());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(requestLogger);
-
-app.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required(),
-      password: Joi.string().required().min(8),
-    }),
-  }),
-  login
-);
-app.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string(),
-      email: Joi.string().required(),
-      password: Joi.string().required().min(8),
-    }),
-  }),
-  createUser
-);
-
-app.use(auth);
-app.use('/users', usersRouter);
-app.use('/movies', moviesRouter);
-app.use(errorLogger);
-app.use(errors());
-app.use(handleErrors);
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемая страница не найдена' });
-});
+app.use(router);
 
 app.listen(3000);
---
+++ routes/index.js
@@ -0,0 +1,51 @@
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { errors, celebrate, Joi } = require('celebrate');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
+const auth = require('../middlewares/auth');
+const { login, createUser } = require('../controllers/users');
+const usersRouter = require('../routes/users');
+const moviesRouter = require('../routes/movies');
+const handleErrors = require('../middlewares/errors');
+const router = require('express').Router();
+
+router.use(cors());
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.use(requestLogger);
+
+router.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  login
+);
+router.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string(),
+      email: Joi.string().required(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser
+);
+
+router.use(auth);
+router.use('/users', usersRouter);
+router.use('/movies', moviesRouter);
+router.use(errorLogger);
+router.use(errors());
+router.use(handleErrors);
+router.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемая страница не найдена' });
+});
+
+module.exports = router;
