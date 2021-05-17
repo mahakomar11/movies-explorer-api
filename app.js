01ac0f1c6a7f381b17f8fc808b3dd6d67745763b
Maria Komarova <komarova@phystech.edu>
2021-05-17 14:23:25 +0300

    Move all to app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
-const router = require('./routes/index')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { errors, celebrate, Joi } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const auth = require('./middlewares/auth');
+const { login, createUser } = require('./controllers/users');
+const usersRouter = require('./routes/users');
+const moviesRouter = require('./routes/movies');
+const handleErrors = require('./middlewares/errors');
+const CustomError = require('./utils/errors');
 
 const app = express();
 
@@ -17,6 +26,43 @@ mongoose.connect(
   }
 );
 
-app.use(router);
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  login
+);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser
+);
+
+app.use(auth);
+app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
+app.use(errors());
+app.use('*', (req, res, next) => {
+  next(new CustomError(404, 'Запрашиваемая страница не найдена'));
+});
+app.use(errorLogger);
+app.use(handleErrors);
 
 app.listen(3000);
